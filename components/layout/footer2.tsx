d04ff70328ad6810e23f74239f687de2cd73fd84
Babar Khan <52751757+bobiiii@users.noreply.github.com>
2024-01-12 19:58:46 +0500

    #

--- components/layout/footer2.tsx
+++ components/layout/footer2.tsx
@@ -6,6 +6,9 @@ import {
 } from "@/components/ui/accordion";
 import Link from 'next/link';
 import LogoSquare from '../logo-square';
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
 
 
 const { COMPANY_NAME, SITE_NAME } = process.env;
@@ -13,7 +16,8 @@ export default async function Footer2() {
     return (
         <footer className=" py-6 bg-[#f0f0f0]">
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 border border-green-400 '>
-                <div className='border border-red-400 '>
+                {/* logo adn adress */}
+                <div className='border border-red-400 text-xs'>
                     <Link className="h-24 border border-black flex items-center gap-2 text-black dark:text-white md:pt-1" href="/">
                         <LogoSquare />
 
@@ -24,11 +28,11 @@ export default async function Footer2() {
                         <span>Fb</span>
                         <span>Insta</span>
                     </div>
-
                 </div>
-                <div className='border border-red-400'>
+                {/* Information */}
+                <div className='border border-red-400 text-xs'>
                     <div className="hidden md:block ">
-                        <span className="text-base lg:text-lg font-semibold block mb-4">INFORMATION</span>
+                        <span className="text-sm lg:text-base font-semibold block mb-2">INFORMATION</span>
                         <ul>
                             <li>
                                 About Us
@@ -43,7 +47,6 @@ export default async function Footer2() {
                         </ul>
 
                     </div>
-
                     <Accordion type="single" collapsible className="md:hidden">
                         <AccordionItem value="item-1">
                             <AccordionTrigger>INFORMATION</AccordionTrigger>
@@ -63,13 +66,12 @@ export default async function Footer2() {
                             </AccordionContent>
                         </AccordionItem>
                     </Accordion>
-
-
                 </div>
-                <div className='border border-red-400'>
+                {/* Collection*/}
+                <div className='border border-red-400 text-xs'>
                     <div className="hidden md:block ">
-                        <span className="text-base lg:text-lg font-semibold block mb-4">CUSTOMER SUPPORT</span>
-                        <ul>
+                        <span className="text-xs lg:text-base font-semibold block mb-2">CUSTOMER SUPPORT</span>
+                        <ul className="">
                             <li>
                                 Contact Us
                             </li>
@@ -83,7 +85,6 @@ export default async function Footer2() {
                                 FAQ's
                             </li>
                         </ul>
-
                     </div>
 
                     <Accordion type="single" collapsible className="md:hidden">
@@ -107,12 +108,14 @@ export default async function Footer2() {
                             </AccordionContent>
                         </AccordionItem>
                     </Accordion>
-
-
                 </div>
-                <div className='border border-red-400'>
+                {/* newsletter */}
+                <div className='border border-red-400 text-xs'>
+                    <span className="text-xs lg:text-base font-semibold block mb-4">NEWSLETTER </span>
+                    <Label htmlFor="email-box">Receive our latest updates</Label>
+                    <div className="flex"><Input id={"email-box"} />
+                        <Button variant="outline" className="bg-customYellow text-white">Button</Button></div>
 
-                    <span className="text-base lg:text-lg font-semibold block mb-4">NEWSLETTER SIGNUP</span>
                 </div>
 
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
+        customYellow:"#d5ac55",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         customColor: "#d5ac55",
